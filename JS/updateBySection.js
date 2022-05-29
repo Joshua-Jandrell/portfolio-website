373ba5e5bd88358783f3937bfdda8c8109e7604a
Joshua-Jandrell <2333213@students.wits.ac.za>
2022-05-29 14:39:36 +0200

    Logging system is now updated

--- JS/updateBySection.js
+++ JS/updateBySection.js
@@ -3,22 +3,79 @@
 
 // costants
 const offsetMult = 0.5; // how far down the window must a dive be for the scroll offset to trigger
-const ScrollMarkrs = [];
+const scrollMarkerGroups = [];
 const markerIdNavSuffex = "-nav-elem";
 // glabal scope varables
 let currentMarker = "none"; // the id of the marker the user has currently scrolled to.
 
-function SetScrollPointMarkers(markerClass) {
+// Setup ===========================
+function SetScrollPointMarkers(markerClass, addedClass, sameAsTarget = false) {
+  console.log(markerClass);
   let markerArray = Array.from(document.getElementsByClassName(markerClass));
-  markerArray.forEach((marker) => {
-    ScrollMarkrs.push(marker);
-  });
+  let markergroup = new MarkerGroup(markerArray, addedClass, sameAsTarget);
+  scrollMarkerGroups.push(markergroup);
+  window.addEventListener("scroll", (e) => markergroup.UpdateMarker());
 }
-
+// Useful Calsses =================
+class MarkerGroup {
+  constructor(markers, addedClass, sameAsTarget) {
+    this.markers = markers;
+    this.addedClass = addedClass;
+    this.sameAsTarget = sameAsTarget;
+    this.currentMarker = "none";
+  }
+  UpdateMarker() {
+    let newCurrent = GetCurrentMarker(this.markers);
+    if (newCurrent != this.currentMarker) {
+      if (this.sameAsTarget) {
+        ToggleClasses(newCurrent, this.currentMarker, this.addedClass);
+      } else {
+        ToggleNavClasses(newCurrent, this.currentMarker, this.addedClass);
+      }
+      this.currentMarker = newCurrent;
+    }
+  }
+}
+// CSS Class updating =================
+function ToggleNavClasses(newElem, oldElem, togglClass) {
+  let newNavId = GetMarkerNavId(newElem.id);
+  let newNav = document.getElementById(newNavId);
+  let oldNavId = GetMarkerNavId(oldElem.id);
+  let oldNav = document.getElementById(oldNavId);
+  ToggleClasses(newNav, oldNav, togglClass);
+}
+function ToggleClasses(newElem, oldElem, toggleClass) {
+  AddClass(newElem, toggleClass);
+  RemoveClass(oldElem, toggleClass);
+}
+function AddClass(elem, addedClass) {
+  if (elem != "none" && elem != null) {
+    if (!elem.classList.contains(addedClass)) {
+      elem.classList.add(addedClass);
+    }
+  }
+}
+function RemoveClass(elem, classToRemove) {
+  if (elem != "none" && elem != null) {
+    if (elem.classList.contains(classToRemove)) {
+      elem.classList.remove(classToRemove);
+    }
+  }
+}
 // Scroll position checking =================
+function GetCurrentMarker(markers) {
+  let currMarker = "none";
+  markers.forEach((marker) => {
+    if (IsVIsisble(marker)) {
+      currMarker = marker;
+    }
+  });
+  return currMarker;
+}
 function IsVIsisble(el) {
   let rect = el.getBoundingClientRect();
   pos = rect.top;
+  console.log(pos + "vs " + GetOffset());
   return pos <= GetOffset();
 }
 // Get offset dynamically to accounted for window re-sizing
